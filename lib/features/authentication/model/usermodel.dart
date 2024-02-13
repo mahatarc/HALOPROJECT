1033f8cc4e90c3efbb32a14e5b3c0f96898dc4a5
zyszysss <archanamahat252@gmail.com>
2024-02-13 20:04:34 +0545

    n

--- lib/features/authentication/model/usermodel.dart
+++ lib/features/authentication/model/usermodel.dart
@@ -2,19 +2,18 @@ class UserModel {
   final String email;
   final String name;
   final String role;
-  
 
   UserModel({
     required this.email,
     required this.name,
-     required this.role,
+    required this.role,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'email': email,
-      'firstname': name,
-       'role': role,
+      'name': name,
+      'role': role,
     };
   }
 }
